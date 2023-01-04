153fcf5f910a7791770a14950f4b810c946ff79c
Deshayes dayann <dayanndeshayes@gmail.com>
2023-01-04 15:41:06 +0100

    [feat] first commit

---
+++ src/main/java/com/simplon/projet_fil_rouge/ProjetFilRougeApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.projet_fil_rouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFilRougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFilRougeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplon/projet_fil_rouge/ProjetFilRougeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.projet_fil_rouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFilRougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
